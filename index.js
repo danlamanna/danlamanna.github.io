2b3ce8642622871c54de9332eac1ca65c6bc49ec
Dan LaManna <dan.lamanna@kitware.com>
2021-02-26 16:10:10 -0500

    Reformat file

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-import axios from "axios";
-import * as Sentry from "@sentry/browser";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import axios from 'axios';
+import * as Sentry from '@sentry/browser';
 
 Sentry.init({
-  dsn:
-    "https://de6deb1db67748989fc0c5e54b1ded5b@o155723.ingest.sentry.io/5652741",
+  dsn: 'https://de6deb1db67748989fc0c5e54b1ded5b@o155723.ingest.sentry.io/5652741',
 });
 
 const params = new URL(document.location).searchParams;
-const queue = params.get("queue") || "na";
+const queue = params.get('queue') || 'na';
 
-document.getElementsByTagName("body")[0].style.color =
-  params.get("color") || "black";
-document.getElementsByTagName("body")[0].style["font-size"] = `${
-  (params.get("size") || 8) * 10
-}px`;
+document.getElementsByTagName('body')[0].style.color = params.get('color') || 'black';
+document.getElementsByTagName('body')[0].style['font-size'] = `${(params.get('size') || 8) * 10}px`;
 
 async function getPlayerCount(queue) {
   let resp, url;
 
-  if (queue === "eu") {
-    url =
-      "https://api.faceit.com/queue/v1/queue/hub/74caad23-077b-4ef3-8b1d-c6a2254dfa75";
-  } else if (queue === "na") {
-    url =
-      "https://api.faceit.com/queue/v1/queue/hub/748cf78c-be73-4eb9-b131-21552f2f8b75";
+  if (queue === 'eu') {
+    url = 'https://api.faceit.com/queue/v1/queue/hub/74caad23-077b-4ef3-8b1d-c6a2254dfa75';
+  } else if (queue === 'na') {
+    url = 'https://api.faceit.com/queue/v1/queue/hub/748cf78c-be73-4eb9-b131-21552f2f8b75';
   }
 
   resp = await axios.get(url);
@@ -33,7 +27,7 @@ async function getPlayerCount(queue) {
 }
 
 async function updatePlayerCounter() {
-  document.getElementById("counter").innerHTML = await getPlayerCount(queue);
+  document.getElementById('counter').innerHTML = await getPlayerCount(queue);
 }
 
 updatePlayerCounter();
