1d2faf80ae9cc340df7d923083402979bdf8be10
Dan LaManna <dan.lamanna@kitware.com>
2021-02-26 16:10:00 -0500

    Some refactoring

--- index.js
+++ index.js
@@ -8,40 +8,33 @@ Sentry.init({
     "https://de6deb1db67748989fc0c5e54b1ded5b@o155723.ingest.sentry.io/5652741",
 });
 
+const params = new URL(document.location).searchParams;
+const queue = params.get("queue") || "na";
+
+document.getElementsByTagName("body")[0].style.color =
+  params.get("color") || "black";
+document.getElementsByTagName("body")[0].style["font-size"] = `${
+  (params.get("size") || 8) * 10
+}px`;
+
 async function getPlayerCount(queue) {
-  let resp;
+  let resp, url;
+
   if (queue === "eu") {
-    resp = await axios.get(
-      "https://api.faceit.com/queue/v1/queue/hub/74caad23-077b-4ef3-8b1d-c6a2254dfa75"
-    );
+    url =
+      "https://api.faceit.com/queue/v1/queue/hub/74caad23-077b-4ef3-8b1d-c6a2254dfa75";
   } else if (queue === "na") {
-    resp = await axios.get(
-      "https://api.faceit.com/queue/v1/queue/hub/748cf78c-be73-4eb9-b131-21552f2f8b75"
-    );
-  } else {
-    throw new Error(`Unknown queue: ${queue}.`);
+    url =
+      "https://api.faceit.com/queue/v1/queue/hub/748cf78c-be73-4eb9-b131-21552f2f8b75";
   }
 
+  resp = await axios.get(url);
   return resp.data.payload[0].noOfPlayers;
 }
 
-function writeCssTag(params) {
-  document.getElementsByTagName("body")[0].style.color =
-    params.get("color") || "black";
-  document.getElementsByTagName("body")[0].style["font-size"] = `${
-    (params.get("size") || 8) * 10
-  }px`;
-}
-
-async function main() {
-  let params = new URL(document.location).searchParams;
-  let queue = params.get("queue");
-
-  writeCssTag(params);
-
+async function updatePlayerCounter() {
   document.getElementById("counter").innerHTML = await getPlayerCount(queue);
 }
 
-main();
-
-setInterval(main, 30000);
+updatePlayerCounter();
+setInterval(updatePlayerCounter, 30000);
